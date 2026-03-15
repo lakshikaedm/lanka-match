42f55706c9ce3b5b80dd4c2c1dc3a6492159fd36
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2026-03-15 15:43:28 +0900

    chore(test): remove Minitest setup and simplify CI
    - delete generated test files and fixtures under test/
    - disable automatic test file generation in Rails generators
    - remove unused test job from GitHub Actions

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module LankaMatch
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    # stop auto generation of mini-tests
+    config.generators do |g|
+      g.test_framework nil
+    end
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
