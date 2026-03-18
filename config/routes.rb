37874bfc7de614e2096e6209b4ac91accd19dc2d
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2026-03-18 23:02:09 +0900

    feat(routes): add public profile browsing routes (index, show)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resource :profile, only: %i[show new create edit update]
-
   root "home#index"
+
+  devise_for :users
+
+  resource :profile, only: %i[show new create edit update]
+  resources :profiles, only: %i[index show], controller: "public_profiles", as: :public_profiles
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
