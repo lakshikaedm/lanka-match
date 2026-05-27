cba4e7de20ab119830f320965e3653c0b8468a7e
Lakshika Sandaruwan <lakshikaedm@gmail.com>
2026-05-27 19:39:13 +0900

    feat(likes): create and remove matches from likes

--- app/models/match.rb
+++ app/models/match.rb
@@ -6,15 +6,29 @@ class Match < ApplicationRecord
   validate :users_must_be_different
   validate :ordered_user_pair
 
+  def self.create_between(user_a, user_b)
+    user_one, user_two = [ user_a, user_b ].sort_by(&:id)
+
+    find_or_create_by(user_one: user_one, user_two: user_two)
+  end
+
+  def self.find_between(user_a, user_b)
+    user_one, user_two = [ user_a, user_b ].sort_by(&:id)
+
+    find_by(user_one: user_one, user_two: user_two)
+  end
+
   private
 
   def users_must_be_different
+    return if user_one_id.blank? || user_two_id.blank?
+
     errors.add(:user_two, "can't be the same user") if user_one_id == user_two_id
   end
 
   def ordered_user_pair
     return if user_one_id.blank? || user_two_id.blank?
 
-    errors.add(:user_one_id, "must be smaller than user_two_id") if user_one_id > user_two_id
+    errors.add(:user_one_id, "must be smaller than user_two_id") unless user_one_id < user_two_id
   end
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -12,10 +12,12 @@ RSpec.describe Match, type: :model do
     it { is_expected.to validate_uniqueness_of(:user_one_id).scoped_to(:user_two_id) }
 
     it "is valid when user_one_id is smaller than user_two_id" do
-      user_one = create(:user)
-      user_two = create(:user)
+      smaller_user = create(:user)
+      larger_user = create(:user)
 
-      match = build(:match, user_one: user_one, user_two: user_two)
+      match = build(:match, user_one: smaller_user, user_two: larger_user)
+
+      expect(smaller_user.id).to be < larger_user.id
       expect(match).to be_valid
     end
 
@@ -37,6 +39,48 @@ RSpec.describe Match, type: :model do
 
       expect(match).not_to be_valid
       expect(match.errors[:user_one_id]).to include("must be smaller than user_two_id")
+    end
+
+    describe ".create_between" do
+      it "creates a match with ordered users" do
+        smaller_user = create(:user)
+        larger_user = create(:user)
+
+        match = described_class.create_between(larger_user, smaller_user)
+
+        expect(match).to be_persisted
+        expect(match.user_one).to eq(smaller_user)
+        expect(match.user_two).to eq(larger_user)
+      end
+
+      it "does not create duplicate matches for the same pair" do
+        user_a = create(:user)
+        user_b = create(:user)
+
+        expect {
+          described_class.create_between(user_a, user_b)
+          described_class.create_between(user_b, user_a)
+        }.to change(described_class, :count).by(1)
+      end
+    end
+
+    describe ".find_between" do
+      it "finds a match regardless of user order" do
+        user_a = create(:user)
+        user_b = create(:user)
+
+        match = described_class.create_between(user_a, user_b)
+
+        expect(described_class.find_between(user_a, user_b)).to eq(match)
+        expect(described_class.find_between(user_b, user_a)).to eq(match)
+      end
+
+      it "returns nil when no match exists" do
+        user_a = create(:user)
+        user_b = create(:user)
+
+        expect(described_class.find_between(user_a, user_b)).to be_nil
+      end
     end
   end
 end
